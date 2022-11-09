cf7a18e2d259d6a3e05cfb5982c1a3be5583d6ff
Carlos Javier <cjangie13@gmail.com>
2022-11-08 22:00:17 -0500

    adding express and first set up

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
+const express = require('express');
 
+const handlers = require('./handlers');
+
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.json({ hello: 'world' });
+});
+
+app.get('/api/login', (req, res) => {});
+
+app.use(handlers.notFound);
+app.use(handlers.errors);
+
+app.listen(port, console.log(`server listening on port ${port}`));
