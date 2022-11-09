67bd3c73180760c7c66bc68a6450b9ba4fc59b8b
Carlos Javier <cjangie13@gmail.com>
2022-11-08 22:38:09 -0500

    fixin structure

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
-const express = require('express');
+require('dotenv').config();
 
+//importing npm package
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+//importing handlers
 const handlers = require('./handlers');
 
 const app = express();
 const port = 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.json({ hello: 'world' });
