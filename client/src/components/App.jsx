696801b9fb2a32a9f1bd1a7eee35714c1e9623f7
Carlos Javier <cjangie13@gmail.com>
2022-11-10 13:01:51 -0500

    adding api methods to client

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useEffect, Component } from 'react';
+import { setToken, call } from '../services/api';
 
-const App = () => <div>App Works</div>;
+const App = () => {
+  useEffect(() => {
+    async function fetchData() {
+      const result = await call('post', 'auth/login', {
+        username: 'kevin',
+        password: 'password',
+      });
 
+      console.log(result);
+    }
+
+    fetchData();
+  }, []);
+
+  return <div>App Works</div>;
+};
+
+// class App extends Component {
+//   async componentDidMount() {
+//     const result = await call('post', 'auth/login', {
+//       username: 'kevin',
+//       password: 'password',
+//     });
+//     console.log(result);
+//   }
+
+//   render() {
+//     return <div>App Works</div>;
+//   }
+// }
 export default App;
