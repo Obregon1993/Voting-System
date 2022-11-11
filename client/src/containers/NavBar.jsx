aaaf7bc7c653e830daf30bc1e1b4132c7bb59f7d
Carlos Javier <cjangie13@gmail.com>
2022-11-11 12:57:01 -0500

    create navbar and handle login and logout

--- client/src/containers/NavBar.jsx
+++ client/src/containers/NavBar.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../store/actions';
 
-const NavBar = () => {
-  return <div>Navbar works</div>;
+const NavBar = ({ auth, logout }) => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <a href="/" onClick={logout}>
+            Logout
+          </a>
+        </li>
+      </ul>
+      {auth.isAuthenticated && <p>Logged in as {auth.user.username}</p>}
+    </div>
+  );
 };
 
-export default NavBar;
+export default connect((store) => ({ auth: store.auth }), { logout })(NavBar);
